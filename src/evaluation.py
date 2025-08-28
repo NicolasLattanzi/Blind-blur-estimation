83da78990d198fbca09880bbad1c4c571846d43a
NicolasLattanzi <150084374+NicolasLattanzi@users.noreply.github.com>
2025-08-28 15:15:16 +0200

    evaluation update

--- src/evaluation.py
+++ src/evaluation.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.data import DataLoader
 
 import data
+import utils
 
 ###### hyper parameters ########
 
@@ -28,9 +29,28 @@ loss_function = torch.nn.MSELoss()
 resnet18.eval()
 GRNN.eval()
 
-for epoch in range(num_epochs):
+def complete_eval():
+    for epoch in range(num_epochs):
+        eval_loss = 0.0
+        print(f'###\t\t  starting epoch n.{epoch+1}  \t\t###\n')
+        for i, (images, blur_types, param1, param2) in enumerate(eval_loader):
+            images = images.to(device)
+            blur_types = blur_types.to(device)
+            param1 = param1.to(device)
+            param2 = param2.to(device)
+            blur_parameters = torch.tensor([[p1,p2] for p1,p2 in zip(param1, param2)], dtype=torch.float32)
+
+            classif_outputs = resnet18(images) # classification
+            final_outputs = GRNN.forward(classif_outputs) # regression
+            loss = loss_function(final_outputs, blur_parameters)
+
+            eval_loss += loss.item()
+
+        avg_eval_loss = eval_loss / eval_size
+        print(f"Epoch [{epoch+1}/{num_epochs}] evaluation completed. Average Loss: {avg_eval_loss:.4f}")
+
+def single_eval():
     eval_loss = 0.0
-    print(f'###\t\t  starting epoch n.{epoch+1}  \t\t###\n')
     for i, (images, blur_types, param1, param2) in enumerate(eval_loader):
         images = images.to(device)
         blur_types = blur_types.to(device)
@@ -43,6 +63,18 @@ for epoch in range(num_epochs):
         loss = loss_function(final_outputs, blur_parameters)
 
         eval_loss += loss.item()
+        break
 
-    avg_eval_loss = eval_loss / eval_size
-    print(f"Epoch [{epoch+1}/{num_epochs}] evaluation completed. Average Loss: {avg_eval_loss:.4f}")
+    for i in range(batch_size):
+        print(i)
+        cout = utils.printable_classif(classif_outputs[i])
+        print(f"blur type: {utils.blur_types[ blur_types[i].item() ]}  /  {cout}")
+        b1 = utils.printable_regr(blur_parameters[i])
+        b2 = utils.printable_regr(final_outputs[i])
+        print(f"paramaters: {b1}  /  {b2}")
+
+    print(f"evaluation loss: {eval_loss:.4f}")
+
+
+single_eval()
+#complete_eval()
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,6 @@
 # dizionario dei tipi, da usare visto che non esistono tensori stringhe
 blur_types = {0: "Gaussian Blur", 1: "Motion Blur", 2: "Defocus Blur"}
 
-
 # estrazione informazioni blur dal path/nome dell'immagine
 def blur_type_from_image_path(path :str):
     filename = path.split('/')[-1]
@@ -11,3 +10,24 @@ def blur_type_from_image_path(path :str):
     blur_param = int(variables[2])
 
     return [blur_type, blur_size, blur_param]
+
+
+############ utility ######################
+
+
+# prende un tensore in input e lo traduce in gaussian, motion o defocus
+# (il tensore è pensato per essere l'output del modello di classificazione)
+def printable_classif( classif_output ):
+    bt = 0
+    blur_value = classif_output[0]
+    for i in range(1, 3):
+        if classif_output[i] > blur_value:
+            bt = i
+            blur_value = classif_output[i]
+    return blur_types[bt]
+
+# denormalizza l'output del modello GRNN
+def printable_regr( regr_output ):
+    p1 = int(regr_output[0] * 128)
+    p2 = int(regr_output[1] * 360)
+    return [p1, p2]
