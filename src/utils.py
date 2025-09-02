34df7de0cb0fc2d6e58e611c244ec1f5fa5a052f
edobughi <168537499+edobughi@users.noreply.github.com>
2025-09-02 18:27:54 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 # dizionario dei tipi, da usare visto che non esistono tensori stringhe
+import os
 blur_types = {0: "Gaussian Blur", 1: "Motion Blur", 2: "Defocus Blur"}
 
 # estrazione informazioni blur dal path/nome dell'immagine
 def blur_type_from_image_path(path :str):
-    filename = path.split('/')[-1]
+    filename = os.path.basename(path)
     variables = filename.split('-')
     blur_type = int(variables[0])
     blur_size = int(variables[1])
