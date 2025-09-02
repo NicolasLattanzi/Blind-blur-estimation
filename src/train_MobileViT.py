2ae3f25529022375c4f467386c2c381e9d42fdcb
edobughi <168537499+edobughi@users.noreply.github.com>
2025-09-02 18:27:09 +0200

    Update train_MobileViT.py

--- src/train_MobileViT.py
+++ src/train_MobileViT.py
@@ -2,23 +2,23 @@ import torch
 from torch.utils.data import DataLoader
 import network
 import data
+import torchvision.transforms as transforms
 
 #parametri
-batch_size = 16
-num_epochs = 8
-learning_rate = 0.001
-
+batch_size = 50
+num_epochs = 13
+learning_rate = 0.0005
 #dataset e separazione in train e test
-dataset = data.BlurDataset(resize=128)
+dataset = data.BlurDataset(resize=224)
 train_dataset, test_dataset = data.train_test_split(dataset)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
 train_size = len(train_loader)
 test_size = len(test_loader)
-
+resize_transform = transforms.Resize((224, 224))
 #modello
-model=network.MobileViT_XS()
+model=network.mobilevit_s()
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 model = model.to(device)
 
@@ -27,12 +27,21 @@ print('//  starting training  //')
 loss_function = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+def resize_if_needed(images, size=(224, 224)):
+    # images: Tensor [batch, channels, height, width]
+    h, w = images.shape[-2], images.shape[-1]
+    if (h, w) != size:
+        # Resize batch: torchvision Resize expects PIL, but can handle tensors since v0.8
+        images = torch.stack([resize_transform(img) for img in images])
+    return images
+
 for epoch in range(num_epochs):
     model.train()
     train_loss=0.0
     print(f'###\t\t  starting epoch n.{epoch+1}  \t\t###\n')
     for i, (images, blur_types, _, _) in enumerate(train_loader):
         images = dataset.augment_data(images)
+        images=resize_if_needed(images)
         images = images.to(device)
         blur_types = blur_types.to(device)
 
@@ -64,5 +73,5 @@ for epoch in range(num_epochs):
     avg_test_loss = test_loss / test_size
     print(f"Epoch [{epoch+1}/{num_epochs}] test completed. Average Loss: {avg_test_loss:.4f}\n")
 
-torch.save(model, 'models/movileViT.pth')
+torch.save(model, 'models/movileViT3.pth')
 
